e84bd680da8ffaa3eec660c33e91c8d26059ffcd
Viet-Vo <viet.allen.qvo@gmail.com>
2018-08-21 00:35:02 -0700

    html/css(bootstrap): 95%   javascript: 10%(outline done)

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+//Global Variables
+// ===================================================================================
+var stat1 = [120, 0, 8];
+var stat2 = [115, 0, 9];
+var stat3 = [100, 0, 12];
+var stat4 = [135, 0, 5];
+var list = [stat1, stat2, stat3, stat4];
+var player = [];
+var opp = [];
+
+var win = 0;
+var loss = 0;
+
+
+// Functions to Call
+// ===================================================================================
+
+
+function startGame() {
+
+}
+
+function selectPlayer() {
+
+}
+
+function selectOpp() {
+
+}
+function tradeBlows() {
+
+}
+
+function pressButton() {
+
+}
+
+
+// Main Process
+// ===================================================================================
+
+
+startGame();
+selectPlayer();
