4bbdaec59111deaca028642ac1df78e91883d30c
Viet-Vo <viet.allen.qvo@gmail.com>
2018-08-21 19:36:40 -0700

    things are bad, things are broken 8.21.2018 2000

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
-// Global Variables
-// ===================================================================================
-var win = 0;
-var loss = 0;
+$(document).ready(function () {
+
+    // Global Variables
+    // ===================================================================================
+    var win = 0;
+    var loss = 0;
 
 
-// Functions to Call
-// ===================================================================================
+    // Functions to Call
+    // ===================================================================================
 
 
+})
 function startGame() {
     var stat1 = [120, 0, 8, "assets/images/evilAbed.jpg"];
     var stat2 = [115, 0, 9, "assets/images/anniesBobs.jpg"];
@@ -16,41 +19,51 @@ function startGame() {
     var list = [stat1, stat2, stat3, stat4];
     var player = [];
     var opp = [];
+    var isSelected = false;
+    var isFightOver = false;
+    console.log(stat1)
+    console.log(stat1[3])
+    $(".playerSelect, .oppSelect").empty()
 }
 
-function selectPlayer() {
-    $(".btn").click(function () {
-    
-    });
-}
+$(".selectChar").on("click", function () {
+    // if (isFightOver) {
+    //     return false;
+    // }
+console.log("test");
+    // if (isSelected) {
+    //     opp = list[value];
+    //     console.log(opp);
+    //     opp[1] = opp[1] + opp[2];
+    //     $("#oppSelect").html(" src="+ stat1[3]);
+    // }
+    // else {
+    //     player = list[value];
+    //     console.log(player);
+    //     $(".playerSelect").html();
+    // }
+});
 
-function selectOpp() {
-    $(".btn").click(function () {
-    
+// function pressButton() {
+//     $(".btn").click(function () {
+//         console.log("test");
+// function tradeBlows() {
+//     player[1] = player[1] + player[2]
+//     player[0] = player[0] - opp[1]
+//     opp[0] = opp[0] - player[1]
+// }
 
-        opp[1] = opp[1] + opp[2];
-    });
-}
-function pressButton() {
-    $(".btn").click(function () {
-        console.log("test");
-        function tradeBlows() {
-            player[1] = player[1] + player[2]
-            player[0] = player[0] - opp[1]
-            opp[0] = opp[0] - player[1]
-        }
+//     });
+// }
 
-    });
-}
 
 // Main Process
 // ===================================================================================
 
 
 startGame();
-selectPlayer();
-selectOpp();
-pressButton();
+// charSelect();
+// pressButton();
 
 
 
